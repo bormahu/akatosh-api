46e6dd6d18d0fed410a5b8ad2c112a06438f70a3
Neil Shevlin <neilshevlin.98@gmail.com>
2021-10-14 11:20:40 +0100

    Update User Controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from 'express'
-import { User } from '../entities/User'
-import {APILogger} from '../utils/logger'
-import { v4 as uuidv4 } from 'uuid'
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../entities/User';
+import {APILogger} from '../utils/logger';
+import { v4 as uuidv4 } from 'uuid';
 
-import { connect } from '../database'
+import { connect } from '../database';
 
 export let getUser = async (req: Request, res:Response, next: NextFunction) => {
   try{
@@ -21,10 +21,10 @@ export let getUser = async (req: Request, res:Response, next: NextFunction) => {
 
     // This seems redundant? if the user is found they will have an ID?
     // This can be removed once we know the response of findOne when there's no such user. 
-    const hasId = await repo.hasId(user)
+    const hasId = await repo.hasId(user);
 
     if (!hasId){
-      return res.status(404).send(`User ${user.username} does not exist`)
+      return res.status(404).send(`User ${user.username} does not exist`);
     }
     return res.status(200).send(user);
 
@@ -54,16 +54,16 @@ export let addUser = async (req:Request, res:Response, next:NextFunction) => {
       account_verified: req.body.account_verified,
       latest_signin: req.body.latest_signin,
     }
-    APILogger.logger.info(`[POST][/users]${user.username}`)
+    APILogger.logger.info(`[POST][/users]${user.username}`);
 
     // Add the user to the DB
-    await repo.save(user)
+    await repo.save(user);
 
-    return res.status(201).send(user)
+    return res.status(201).send(user);
 
   } catch(error) {
-    APILogger.logger.info(`[POST][/users][ERROR]${error}`)
-    return res.status(500).send(error)
+    APILogger.logger.info(`[POST][/users][ERROR]${error}`);
+    return res.status(500).send(error);
   }
 }
 
@@ -72,9 +72,9 @@ export let updateUser = async (req:Request, res:Response, next: NextFunction) =>
     const connection = await connect();
     const repo = connection.getRepository(User);
 
-    const username = req.body.username
-    const user = await repo.findOne({where: {user_name: username}})
-    APILogger.logger.info(`[PATCH][/users]${user}`)
+    const username = req.body.username;
+    const user = await repo.findOne({where: {user_name: username}});
+    APILogger.logger.info(`[PATCH][/users]${user}`);
 
     const has_id = await repo.hasId(user);
 
@@ -90,13 +90,13 @@ export let updateUser = async (req:Request, res:Response, next: NextFunction) =>
     user.user_company = req.body.user_company || user.user_company;
     user.user_type = req.body.user_type || user.user_type;
 
-    await repo.save(user)
+    await repo.save(user);
 
-    return res.status(204).send()
+    return res.status(204).send();
 
   } catch(error) {
-    APILogger.logger.info(`[PATCH][/users][ERROR]${error}`)
-    return res.status(500).send(error)
+    APILogger.logger.info(`[PATCH][/users][ERROR]${error}`);
+    return res.status(500).send(error);
   }
 }
 export let removeUser = async (req:Request, res: Response, next: NextFunction) => {
@@ -115,7 +115,7 @@ export let removeUser = async (req:Request, res: Response, next: NextFunction) =
 
     await repo.delete({username: username});
     APILogger.logger.info(`[DELETE][/users]${username}`);
-    
+
     return res.status(204).send(`User ${username} has been deleted`);
 
   } catch(error) {
