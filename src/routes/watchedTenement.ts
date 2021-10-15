0ad78b34ba63790b585296727c8d7f58c96c74bb
Neil Shevlin <neilshevlin.98@gmail.com>
2021-10-15 17:54:32 +0100

    Update watchlist router name

--- src/routes/watchedTenement.ts
+++ src/routes/watchedTenement.ts
@@ -2,10 +2,10 @@ import * as tenementController from '../controllers/watchedTenement'
 
 export class WatchedTenementRoute{
   public routes(app):void{
-    app.route('/tenement').post(tenementController.addWatchedTenement)
-    app.route('/tenement/:tenementId').patch(tenementController.updateWatchedTenement)
-    app.route('/tenement/:tenementId').delete(tenementController.removeWatchedTenement)
-    app.route('/tenement/').get(tenementController.getWatchedTenements)
+    app.route('/watch').post(tenementController.addWatchedTenement)
+    app.route('/watch/:watchId').patch(tenementController.updateWatchedTenement)
+    app.route('/watch/:watchId').delete(tenementController.removeWatchedTenement)
+    app.route('/watch').get(tenementController.getWatchedTenements)
 
   }
 }
