08815d6bd10a03968e214ad71f3345c014e80715
Neil Shevlin <neilshevlin.98@gmail.com>
2022-01-26 16:35:49 +0000

    Update db congig names

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,16 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config({
-    path:'/Users/neilshevlin/Desktop/akatosh-api/.env'
+    // SET YOUR LOCAL PATH TO YOUR ENV FILE
+    path:'/Users/neilshevlin/Desktop/Akatosh/api/.env'
 })
 // port config
 // export const port = Number(process.env.API_PORT);
 
 // Database config
-export const db_host = String(process.env.POSTGRESQL_DB_HOST);
-export const db_port = Number(process.env.POSTGRESQL_PORT);
-export const db_name = String(process.env.POSTGRESQL_DB);
-export const db_user = String(process.env.POSTGRESQL_DB_USER);
-export const db_password = String(process.env.POSTGRESQL_DB_PASSWORD);
+export const db_host = String(process.env.MYSQL_DB_HOST);
+export const db_port = Number(process.env.MYSQL_PORT);
+export const db_name = String(process.env.MYSQL_DB);
+export const db_user = String(process.env.MYSQL_DB_USER);
+export const db_password = String(process.env.MYSQL_DB_PASSWORD);
 export const secret = String(process.env.JWT_SECRET);
