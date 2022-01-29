1b6cc25db7b51eb60949675a596a73f784b6fd7e
Neil Shevlin <neilshevlin.98@gmail.com>
2022-01-29 15:55:08 +0000

    Update tests with correct flow/data && fix issue regarding authorization spelling

--- test/02_user.spec.ts
+++ test/02_user.spec.ts
@@ -20,14 +20,15 @@ const expect = chai.expect
 
 const user = {
   // Generic values for testing
-  email: 'test@test.com',
-  firstName: 'test',
-  lastName: 'test',
-  password: 'test',
-  userCompany: 'test',
-  userId: uuidv4(),
-  userType: 'admin',
-  username: 'tester',
+  data : {
+    email: 'test@test.com',
+    firstName: 'test',
+    lastName: 'test',
+    password: 'test',
+    userCompany: 'test',
+    userType: 'admin',
+    username: 'tester',
+  }
 }
 
 describe('userRoute', () =>{
@@ -36,8 +37,7 @@ describe('userRoute', () =>{
     expect(User.name).to.be.equal('User')
     // Should create some dummy user here to post in to test login
   })
-  it('should be able to login', async()=>{
-    APILogger.logger.info(`Logging in`)
+  it('should return a 404 because user does not exist', async()=>{
     return chai
     .request(app)
     .post(`/users/login`)
@@ -47,40 +47,51 @@ describe('userRoute', () =>{
       token = res.body.token
     })
   })
-  it('should respond with a status 404 because there is no user', async()=>{
-    return chai
-    .request(app)
-    .get(`/users/${user.username}`)
-    .set('Authorisation', `Bearer ${token}`)
-    .then(res => {
-      expect(res.status).to.be.equal(404)
-    })
-  })
   it('should create a new user and retrieve it back', async()=>{
     return chai
     .request(app)
     .post(`/users`)
-    .set('Authorisation', `Bearer ${token}`)
     .send(user)
     .then(res => {
       expect(res.status).to.be.equal(201)
     })
   })
-  it('should return the user created in the previous post', async()=>{
+  it('should return a 200 because the user now exists and can now login', async()=>{
     return chai
     .request(app)
-    .get(`/users/?username=${user.username}`)
-    .set('Authorisation', `Bearer ${token}`)
+    .post(`/users/login`)
+    .send(user)
     .then(res => {
       expect(res.status).to.be.equal(200)
-      expect(res.body.username).to.be.equal(user.username)
+      token = res.body.token
+    })
+  })
+  it('should return the user created in the previous post', async()=>{
+
+    return chai
+    .request(app)
+    .get(`/users/?username=${user.data.username}`)
+    .set('Authorization', `Bearer ${token}`)
+    .then(res => {
+      expect(res.status).to.be.equal(200)
+      expect(res.body.username).to.be.equal(user.data.username)
+    })
+  })
+  it('should return a 204 after succesfully updating the user', async()=>{
+    return chai
+    .request(app)
+    .patch(`/users`)
+    .set('Authorization', `Bearer ${token}`)
+    .send(user)
+    .then(res => {
+      expect(res.status).to.be.equal(204)
     })
   })
   it('should remove the user added', async()=>{
     return chai
     .request(app)
     .delete(`/users`)
-    .set('Authorisation', `Bearer ${token}`)
+    .set('Authorization', `Bearer ${token}`)
     .send(user)
     .then(res => {
       expect(res.status).to.be.equal(204)
@@ -89,8 +100,8 @@ describe('userRoute', () =>{
   it('should respond with a status 404 again as the user has been deleted', async()=>{
     return chai
     .request(app)
-    .get(`/users/?username=${user.username}`)
-    .set('Authorisation', `Bearer ${token}`)
+    .get(`/users/?username=${user.data.username}`)
+    .set('Authorization', `Bearer ${token}`)
     .then(res => {
       expect(res.status).to.be.equal(404)
     })
