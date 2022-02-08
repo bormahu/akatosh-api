7f6d5e8aba9af59ea78c1f0120eb4ebc8c6234b7
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-08 20:55:03 +0000

    Update env for production

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ dotenv.config({
 // export const port = Number(process.env.API_PORT);
 
 // Database config
-export const db_host = String(process.env.MYSQL_DB_HOST);
-export const db_port = Number(process.env.MYSQL_PORT);
-export const db_name = String(process.env.MYSQL_DB);
-export const db_user = String(process.env.MYSQL_DB_USER);
-export const db_password = String(process.env.MYSQL_DB_PASSWORD);
+export const db_host = String(process.env.RDS_HOSTNAME);
+export const db_port = Number(process.env.RDS_PORT);
+export const db_name = String(process.env.RDS_DB_NAME);
+export const db_user = String(process.env.RDS_USERNAME);
+export const db_password = String(process.env.RDS_PASSWORD);
 export const secret = String(process.env.JWT_SECRET);
