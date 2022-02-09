8ff89a0e86cce0daba7f6a5de981e565b1ba3703
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-09 10:52:19 +0000

    Add winston logs to AWS cloudwatch

--- src/config.ts
+++ src/config.ts
@@ -14,3 +14,6 @@ export const db_name = String(process.env.MYSQL_DB);
 export const db_user = String(process.env.MYSQL_DB_USER);
 export const db_password = String(process.env.MYSQL_DB_PASSWORD);
 export const secret = String(process.env.JWT_SECRET);
+export const logger_access_key_id = String(process.env.LOGGER_ACCESS_KEY_ID);
+export const logger_secret_key = String(process.env.LOGGER_SECRET_KEY);
+export const logger_region = String(process.env.LOGGER_REGION);
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,8 @@
 import { createLogger, format, transports } from 'winston'
+import CloudWatchTransport = require('winston-aws-cloudwatch')
 const { combine, timestamp, label, prettyPrint, printf } = format
+
+import { logger_access_key_id, logger_secret_key, logger_region  } from "../config";
 
 export class APILogger{
     public static myFormat = printf(info =>{
@@ -15,7 +18,21 @@ export class APILogger{
         level:'info',
         transports:[
             new transports.File({filename:'aggregated.log'}),
-            new transports.Console()
+            new transports.Console(),
+            new CloudWatchTransport({
+                logGroupName:'Akatosh-API-logs',
+                logStreamName: 'Akatosh-API-logstream',
+                createLogGroup: false,
+                createLogStream: true,
+                submissionInterval: 2000,
+                submissionRetryCount: 1,
+                batchSize: 20,
+                awsConfig:{
+                    accessKeyId: logger_access_key_id,
+                    secretAccessKey: logger_secret_key,
+                    region: logger_region
+                }
+            })
         ],
 
     })
