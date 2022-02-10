1d6e0d6003838277b6f7d1690524fe19bd3d56c0
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-10 14:34:26 +0000

    Change logger for development

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,5 @@
 import { createLogger, format, transports } from 'winston'
-import CloudWatchTransport = require('winston-aws-cloudwatch')
 const { combine, timestamp, label, prettyPrint, printf } = format
-
-import { logger_access_key_id, logger_secret_key, logger_region  } from "../config";
 
 export class APILogger{
     public static myFormat = printf(info =>{
@@ -18,21 +15,7 @@ export class APILogger{
         level:'info',
         transports:[
             new transports.File({filename:'aggregated.log'}),
-            new transports.Console(),
-            new CloudWatchTransport({
-                logGroupName:'Akatosh-API-logs',
-                logStreamName: 'Akatosh-API-logstream',
-                createLogGroup: false,
-                createLogStream: true,
-                submissionInterval: 2000,
-                submissionRetryCount: 1,
-                batchSize: 20,
-                awsConfig:{
-                    accessKeyId: logger_access_key_id,
-                    secretAccessKey: logger_secret_key,
-                    region: logger_region
-                }
-            })
+            new transports.Console()
         ],
 
     })
