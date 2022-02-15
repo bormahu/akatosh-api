55ccd73090ececd8fe14a28974272528f87289e6
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-15 15:28:57 +0000

    Fix merge conflict

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as expressWinston from 'express-winston';
 import * as morgan from 'morgan';
-import * as passport from 'passport';
 import * as winston from 'winston';
 
 import {APILogger} from './utils/logger';
@@ -14,9 +13,7 @@ import { AreaOfInterestRoute } from './routes/areaOfInterest';
 import { CompanyRoute } from './routes/company';
 import { GlobalTenementRoute } from './routes/globalTenements';
 import { Index } from './routes/index';
-import { UserRoute } from './routes/user';
 import { WatchedTenementRoute } from './routes/watchedTenement';
-import { AuthRoute } from './routes/auth';
 
 dotenv.config({
   path:'/Users/neilshevlin/Desktop/akatosh-api/.env'
@@ -27,25 +24,19 @@ class App {
   public apiRoutes: APIRoute = new APIRoute();
   public indexRoutes:Index= new Index();
   public companyRoutes:CompanyRoute = new CompanyRoute();
-  public userRoutes:UserRoute = new UserRoute();
   public watchedTenementRoutes:WatchedTenementRoute = new WatchedTenementRoute();
   public globalTenementRoutes: GlobalTenementRoute = new GlobalTenementRoute();
   public areaOfInterestRoutes: AreaOfInterestRoute = new AreaOfInterestRoute();
-  public authRoutes: AuthRoute = new AuthRoute();
-
 
   constructor() {
     this.app = express();
     this.apiRoutes.routes(this.app);
     this.app.use(bodyParser.json());
     this.indexRoutes.routes(this.app);
-    this.app.use(passport.initialize());
     this.companyRoutes.routes(this.app);
-    this.userRoutes.routes(this.app);
     this.watchedTenementRoutes.routes(this.app);
     this.globalTenementRoutes.routes(this.app);
     this.areaOfInterestRoutes.routes(this.app);
-    this.authRoutes.routes(this.app);
     // this.app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
     this.app.use(
       expressWinston.errorLogger({
