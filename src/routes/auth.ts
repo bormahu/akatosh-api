6c65a1531f52073cd65f9ca7cd471e3e656a8ad4
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-15 15:07:19 +0000

    Remove auth routes and controllers

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import * as passport from 'passport'
-import * as AuthController from '../controllers/auth';
-import { PassportConfig } from '../utils/passportConfig';
-
-export class AuthRoute extends PassportConfig{
-    public routes(app):void{
-        app.route('/auth/login').post(AuthController.login)
-      }
-}
