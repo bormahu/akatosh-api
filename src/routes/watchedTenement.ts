cc04e593915c69f8b2a6e5d0f0df3cfa58f117f8
Neil Shevlin <neilshevlin.98@gmail.com>
2022-02-16 17:49:10 +0000

    update watch route endpoints

--- src/routes/watchedTenement.ts
+++ src/routes/watchedTenement.ts
@@ -2,25 +2,32 @@ import * as tenementController from '../controllers/watchedTenement';
 import { authMiddleware } from '../utils/auth0';
 export class WatchedTenementRoute{
   public routes(app):void{
-    app.route('/watch').post( 
+    app.route('/watches').post( 
       authMiddleware, 
       tenementController.addWatchedTenement
       )
-    app.route('/watch').patch( 
+    app.route('/watches').patch( 
       authMiddleware, 
       tenementController.updateWatchedTenement
       )
-    app.route('/watch').delete( 
+    app.route('/watches').delete( 
       authMiddleware, 
       tenementController.removeWatchedTenement
       )
-    app.route('/watch/').get( 
-      authMiddleware, 
-      tenementController.getWatchById
+    // Get all watched tenements
+    app.route('/watches').get(
+      authMiddleware,
+      tenementController.getWatches
       )
-    app.route('/watch/owner').get( 
+    // get all watched tenements by user
+    app.route('/watches/owners/:id').get(
+      authMiddleware,
+      tenementController.getWatchesByOwner
+    )
+    // get a specific watched tenement by its id
+    app.route('/watches/:id').get( 
       authMiddleware, 
-      tenementController.getWatchesByOwnerId
+      tenementController.getWatch
       )
   }
 }
